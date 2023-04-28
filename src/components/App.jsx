ed9251a4eebb7bb3d781d0dd5f0331a9955da87c
TetianaTkalich <114866505+TetianaTkalich@users.noreply.github.com>
2023-04-28 23:10:10 +0300

    Test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,11 @@ import { Profile } from '../components/profile/profile';
 import { Statistic } from './statistic/statistic';
 import { FriendList } from './friends/friendsList';
 import { TransactionHistory } from './transaction/transaction';
-
+///JSON UPLOAD///
 import user from '../components/json/user.json';
 import data from '../components/json/data.json';
 import friends from '../components/json/friends.json';
 import transactions from '../components/json/transactions.json';
-
 
 export const App = () => {
   return (
